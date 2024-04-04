638fcc295bffeb81839c00ef66c5f3166e801b1c
Lauren Perez <laurenashleymarks@gmail.com>
2024-04-03 23:42:04 -0700

    adds starter code

--- src/basic-functions.js
+++ src/basic-functions.js
@@ -1,34 +1,25 @@
 // validate an email address
 function validateEmail(email) {
-    const re = /\S+@\S+\.\S+/;
-    return re.test(email);
+  // Check if the email contains the '@' symbol and a '.'
+  if (email.includes("@") && email.includes(".")) {
+    return true;
+  }
+  return false;
 }
 
 // validate a password on complexity requirements
 function validatePassword(password) {
-    return password.length >= 8;
+  return password.length >= 8;
 }
 
 // validate a phone number
 function validatePhoneNumber(phoneNumber) {
-    const re = /^\d{10}$/;
-    return re.test(phoneNumber);
-}
-
-// function to convert data between different formats (object to JSON)
-function convertToJSON(data) {
-    return JSON.stringify(data);
-}
-
-// Function to sanitize user input or escape special characters to prevent injection attacks.
-function sanitizeInput(input) {
-    return input.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+  const re = /^\d{10}$/;
+  return re.test(phoneNumber);
 }
 
 module.exports = {
-    validateEmail,
-    validatePassword,
-    validatePhoneNumber,
-    convertToJSON,
-    sanitizeInput
+  validateEmail,
+  validatePassword,
+  validatePhoneNumber,
 };
